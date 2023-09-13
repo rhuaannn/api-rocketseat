6c9ac7d56238ba91241218a3f7cb6cf50310de41
rhuaannn <rhuanmarques1223@gmail.com>
2023-09-13 20:21:55 -0300

    feat: api basic

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
 const express = require("express");
 
 const app = express();
+
+app.use(express.json())
+
+
 //parametros da rota usamos : e os parametros
 //são obrigatorios como o ex de codigo abx
-app.get("/users/:id/:user", (request, response) => {
-    const { id, user} = request.params;
+// app.get("/users/:id/:user", (request, response) => {
+//     const { id, user} = request.params;
 
-    response.send(`ID do usuário: ${id} ${user}`);
-});
+//     response.send(`ID do usuário: ${id} ${user}`);
+// });
 
 
 //query params são parametros que e acessado atraves de ? e &
 //sao opcionais
-app.get("/users",(request, response) =>{
-    const { page, limit } = request.query;
-    response.send ( `Pagina ${page} limite ${limit}`)
+app.post("/users",(request, response) =>{
+    const { name, email,password } = request.body;
+   response.json({name, email, password})
 
 })
 
-const PORT = 3333;
+const PORT = 3332;
 
-app.listen(PORT, () => {
-    console.log(`server is running on PORT => ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
