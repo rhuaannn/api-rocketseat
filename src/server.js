395a41b139d4fe3845d198dfddcdab6cb1a48846
rhuaannn <rhuanmarques1223@gmail.com>
2023-09-13 20:43:17 -0300

    feat: estrutura do projeto

--- src/server.js
+++ src/server.js
@@ -1,27 +1,12 @@
 const express = require("express");
 
+const routes = require("./routes");
+
+
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
+app.use(routes);
 
-
-//parametros da rota usamos : e os parametros
-//são obrigatorios como o ex de codigo abx
-// app.get("/users/:id/:user", (request, response) => {
-//     const { id, user} = request.params;
-
-//     response.send(`ID do usuário: ${id} ${user}`);
-// });
-
-
-//query params são parametros que e acessado atraves de ? e &
-//sao opcionais
-app.post("/users",(request, response) =>{
-    const { name, email,password } = request.body;
-   response.json({name, email, password})
-
-})
-
-const PORT = 3332;
-
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
