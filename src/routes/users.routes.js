61eeb07b5cdc8932decaf619db409cbdbde45b0f
rhuaannn <rhuanmarques1223@gmail.com>
2023-09-21 14:11:41 -0300

    feat: add knex in project.

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,20 +1,14 @@
 const { Router  } = require("express");
 
-const usersRoutes = Router();
 const UsersController = require("../controllers/UsersController");
 
+const usersRoutes = Router();
 
 
-function myMiddleware(req, res, next){
-
-    if(!req.body.isAdmin){
-        return res.json({message: "user unauthorized"})
-    }
-    next()
-}
 
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
